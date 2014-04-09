3965c93b6b4c311a5505b62b91c629931193c1cd
rosh <rosh@balink.net>
2014-04-09 10:49:48 +0300

    GREEN: FizzBuzz passed

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -14,6 +14,8 @@ namespace FizzBuzz
 
         public string FizzBuzz(int num)
         {
+            if (num % 3 == 0 && num % 5 == 0)
+                return "FizzBuzz";
             if (num % 3 == 0)
                 return "Fizz";
             if (num % 5 == 0)
