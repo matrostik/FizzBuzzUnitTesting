d2c41714057303fcd8157af04a4dd862763626b2
rosh <rosh@balink.net>
2014-04-09 10:38:08 +0300

    GREEN: Buzz passed

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -16,6 +16,8 @@ namespace FizzBuzz
         {
             if (num % 3 == 0)
                 return "Fizz";
+            if (num % 5 == 0)
+                return "Buzz";
             return num.ToString();
         }
     }
